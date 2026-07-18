63c36aa56bd41ef93b235edb96361217fd44850c
Indivdual-01 <thethingtd@gmail.com>
2026-07-18 11:37:29 -0400

    Connect Peterborough street map

--- src/Map.jsx
+++ src/Map.jsx
@@ -3,90 +3,63 @@ import maplibregl from "maplibre-gl";
 
 import "maplibre-gl/dist/maplibre-gl.css";
 
-import neonMapStyle from "./neon-map-style.js";
-
 function Map() {
-
   const mapContainer = useRef(null);
   const map = useRef(null);
 
-
   useEffect(() => {
-
     if (map.current) return;
 
-
     map.current = new maplibregl.Map({
-
       container: mapContainer.current,
 
-      // Temporary map style
-      // We will replace this with the custom neon street style
-      // after connecting the real map data
-     style: neonMapStyle,
-      // Peterborough Ontario
-      center: [
-        -78.3197,
-        44.3091
-      ],
+      // Real street and road map
+      style: "https://tiles.openfreemap.org/styles/liberty",
+
+      // Peterborough, Ontario
+      center: [-78.3197, 44.3091],
 
       zoom: 12,
-
-      minZoom: 10,
-
+      minZoom: 9,
       maxZoom: 19,
-
       pitch: 0,
-
       bearing: 0
-
     });
 
-
-    // Zoom + navigation controls
     map.current.addControl(
       new maplibregl.NavigationControl(),
       "top-right"
     );
 
+    map.current.addControl(
+      new maplibregl.FullscreenControl(),
+      "top-right"
+    );
 
-    // Add a marker for the center of Peterborough
     new maplibregl.Marker({
       color: "#00b7ff"
     })
-      .setLngLat([
-        -78.3197,
-        44.3091
-      ])
+      .setLngLat([-78.3197, 44.3091])
+      .setPopup(
+        new maplibregl.Popup().setHTML(
+          "<strong>Peterborough, Ontario</strong>"
+        )
+      )
       .addTo(map.current);
 
+    map.current.on("error", (event) => {
+      console.error("Map loading error:", event.error);
+    });
 
     return () => {
-
       if (map.current) {
-
         map.current.remove();
-
         map.current = null;
-
       }
-
     };
-
-
   }, []);
 
-
-  return (
-
-    <div
-      ref={mapContainer}
-      className="map"
-    />
-
-  );
-
+  return <div ref={mapContainer} className="map" />;
 }
-
 
 export default Map;
