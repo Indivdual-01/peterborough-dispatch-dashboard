efa858fb103b384b7db636285a5e7227b9346aa4
Indivdual-01 <thethingtd@gmail.com>
2026-07-17 23:20:36 -0400

    Update neon-map-style.js

--- src/neon-map-style.js
+++ src/neon-map-style.js
@@ -1,1 +1,21 @@
+const neonMapStyle = {
+  version: 8,
 
+  name: "Peterborough Neon Map",
+
+  sources: {},
+
+  layers: [
+
+    {
+      id: "background",
+      type: "background",
+      paint: {
+        "background-color": "#050505"
+      }
+    }
+
+  ]
+};
+
+export default neonMapStyle;
